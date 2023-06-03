a62a3b638ce5fa1a4db971a3be838846eec6d7b0
redth <jondick@gmail.com>
2023-06-03 11:50:41 -0400

    Try humidity instead for pressure

--- src/filterPressureAccessoryHandler.ts
+++ src/filterPressureAccessoryHandler.ts
@@ -36,8 +36,8 @@ export class FilterPressureAccessoryHandler implements PoolMathAccessoryHandler 
 		// Temp sensor
 		const filterPressureServiceName = 'Filter Pressure';
 		this.filterPressureService = this.accessory.getService(filterPressureServiceName)
-			|| this.accessory.addService(this.platform.Service.LightSensor, filterPressureServiceName, 'FilterPressurePool');
-		this.filterPressureService.getCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel)
+			|| this.accessory.addService(this.platform.Service.HumiditySensor, filterPressureServiceName, 'FilterPressurePool');
+		this.filterPressureService.getCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity)
 			.onGet(() => this.getFilterPressure());
 	}
 
@@ -50,7 +50,7 @@ export class FilterPressureAccessoryHandler implements PoolMathAccessoryHandler 
 		}
 
 		const pressure = this.controller.status.Pressure;
-		this.filterPressureService.updateCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel, pressure);
+		this.filterPressureService.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, pressure);
 	}
 
 	getFilterPressure () : Nullable<CharacteristicValue> {
